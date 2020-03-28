b95c199401df0777980581f60a5eef36c09894eb
huanggengzhong <915835528@qq.com>
2020-03-28 18:53:58 +0800

    怎么去使用React Context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 // import StateDemo from'./components/StateDemo';
 // import UncontrolledDemo from'./components/UncontrolledDemo';
-import PortalsDemo from'./components/PortalsDemo';
+// import PortalsDemo from'./components/PortalsDemo';
+import ContextDemo from './components/ContextDemo';
 
 function App() {
   return (
     <div>
       {/* <StateDemo/> */}
       {/* <UncontrolledDemo/> */}
-      <PortalsDemo> Modal内容 </PortalsDemo>
+      {/* <PortalsDemo> Modal内容 </PortalsDemo> */}
+      <ContextDemo/>
     </div>
   );
 }
