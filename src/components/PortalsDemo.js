526b260a499f775d3994fced9953ee865eba6e44
huanggengzhong <915835528@qq.com>
2020-03-28 13:09:52 +0800

    使用Portals

--- src/components/PortalsDemo.js
+++ src/components/PortalsDemo.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDom from 'react-dom';
 import './style.css';
 
 
@@ -11,12 +12,17 @@ class PortalsDemo extends React.Component {
   }
   render() {
 
-    // 正常渲染
-    return(
-      <div className="modal1">
-          {this.props.children} {/*这个类似于 vue 的 slot */}
-      </div>
-    )
+    // // 正常渲染
+    // return(
+    //   <div className="modal1">
+    //       {this.props.children} {/*这个类似于 vue 的 slot */}
+    //   </div>
+    // )
+
+    // 使用Portals渲染到body上.
+    // fixed元素要放在body上,有更好的浏览器兼容性.
+    return ReactDom.createPortal(<div className="modal1">{this.props.children}</div>,document.body) //第二参数是DOM节点
+
 
   }
 }
